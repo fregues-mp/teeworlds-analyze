271cdb8e3e4c2e6ccb54f566edbf7f05e6df6ed3
Tfregues <rafaelitoto895@gmail.com>
2024-11-01 01:24:54 -0300

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,11 +1,10 @@
 const teeworlds = require('teeworlds');
-const { logMessage } = require('../src/logger');
 
 let client = new teeworlds.Client("26.200.146.224", 8303, "name", {
     identity: {
         "name": "πeis bot",
         "clan": "πeis ∲",
-        "skin": "santa_psychowolfe'",
+        "skin": "santa_psychowolfe",
         "use_custom_color": 1,
         "color_body": 13631488,
         "color_feet": 14090240,
@@ -15,39 +14,53 @@ let client = new teeworlds.Client("26.200.146.224", 8303, "name", {
 
 client.connect().catch((error) => {
     console.error("Failed to connect:", error);
-    logMessage(`Failed to connect: ${error.message}`);
     process.exit(1);
 });
 
 client.on("connected", async () => {
     console.log("Connected!");
-    await logMessage("Connected!");
-    client.game.SetTeam(-1);
 });
 
 client.on("message", async (message) => {
     console.log("Message from server:", message); 
-    await logMessage(`Message from server: ${JSON.stringify(message)}`);
 });
 
 client.on("kill", async (message) => {
     console.log("Kill event:", message);
-    await logMessage(`Kill event: ${JSON.stringify(message)}`);
 });
 
 client.on("disconnect", async (message) => {
     console.log("Disconnected: ", message);
-    await logMessage(`Disconnected: ${JSON.stringify(message)}`);
 });
 
-process.stdin.on("data", async (data) => {
-    const command = data.toString().trim();
-    if (command === 'quit') {
-        await client.Disconnect();
-        console.log("Disconnected from server.");
-        await logMessage("Disconnected from server.");
-        process.exit(0);
-    } else {
-        client.game.Say(command);
-    }
+process.on("SIGINT", async () => {
+    console.log("Shutting down bot...");
+    isShuttingDown = true;
+
+    const maxAttempts = 10;
+    let attempts = 0;
+
+    const disconnectClients = async () => {
+        for (const client of clients) {
+            try {
+                if (client && typeof client.Disconnect === 'function') {
+                    await client.Disconnect();
+                    console.log("Disconnected");
+                }
+            } catch (error) {
+                console.error(`Error during disconnection: ${error.message}`);
+            }
+        }
+
+        attempts++;
+
+        if (attempts < maxAttempts) {
+            setTimeout(disconnectClients, 1000);
+        } else {
+            console.log("Bot has been shut down.");
+            process.exit(0);
+        }
+    };
+
+    disconnectClients();
 });

de01b7caba9ee33bc3c7290f2030baf7fa886197
Tfregues <rafaelitoto895@gmail.com>
2024-11-04 01:14:42 -0300

    Create multbot.js

---
+++ src/bots/default-analyze/multbot.js
@@ -0,0 +1,27 @@
+const { fork } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+
+const configPath = path.join(__dirname, '../../servers/MyServers.json');
+const config = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
+
+function startBot(server) {
+    const { Name, ip, ports } = server;
+    const botScript = path.join(__dirname, 'bot.js');
+    
+    ports.forEach(port => {
+        const botProcess = fork(botScript, [ip, port, Name]);
+
+        botProcess.on('message', (message) => {
+            console.log(`Bot [${ip}:${port}] - ${message}`);
+        });
+
+        botProcess.on('exit', (code) => {
+            console.log(`Bot [${ip}:${port}] exited with code ${code}`);
+        });
+    });
+}
+
+config.servers.forEach(server => {
+    startBot(server);
+});
